b5d3ec1fc34737422fc04191bf22a69b4fe1fcee
Amusat Gifty <amusatgifty@gmail.com>
2025-10-06 09:40:08 +0100

    updated index page

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://blogback-97zr.onrender.com/api';
+const BASE_URL = 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -279,7 +279,6 @@
 </div>
 </template>
 
-
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
